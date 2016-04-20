3301c4bcdd3eb8db51f17470b6309dfab6987605
Marina <marina.andenko@gmail.com>
2016-04-20 14:06:04 +0300

    host changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host => 'mybooks777.herokuapp.com' }
+  config.action_mailer.default_url_options = { host => 'booksgallery.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
